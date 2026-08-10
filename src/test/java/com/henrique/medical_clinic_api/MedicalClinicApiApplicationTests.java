c3d3e038eb37b8f513c3e2783e9dee0dcd8b62f7
Henrique Sousa <henriquesousaoliveira281@gmail.com>
2026-08-10 10:41:43 -0300

    chore: initialize spring boot project structure

---
+++ src/test/java/com/henrique/medical_clinic_api/MedicalClinicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henrique.medical_clinic_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalClinicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
