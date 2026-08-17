af3a53b3c0f3e1eda75bc11e2573d5dc59679d3c
Henrique Sousa <henriquesousaoliveira281@gmail.com>
2026-08-17 10:23:32 -0300

    feat: add new consultation status values

--- src/main/java/com/henrique/medical_clinic_api/model/Status.java
+++ src/main/java/com/henrique/medical_clinic_api/model/Status.java
@@ -1,7 +1,12 @@
 package com.henrique.medical_clinic_api.model;
 
 public enum Status {
-    SCHEDULED("scheduled");
+    SCHEDULED("scheduled"),
+    CONFIRMED("confirmed"),
+    ON_HOLD("on hold"),
+    BEING_SERVED("being served"),
+    CARRIED_OUT("carried out"),
+    CANCELED("canceled");
 
     private final String name;
     Status(String name) {
