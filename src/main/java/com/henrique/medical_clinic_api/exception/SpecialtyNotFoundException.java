76354b24bfb5b4b3f7dc554e0f9d9638f3d648fa
Henrique Sousa <henriquesousaoliveira281@gmail.com>
2026-08-21 11:30:01 -0300

    feat: add SpecialtyNotFoundException

--- src/main/java/com/henrique/medical_clinic_api/exception/SpecialtyNotFoundException.java
+++ src/main/java/com/henrique/medical_clinic_api/exception/SpecialtyNotFoundException.java
@@ -1,7 +1,10 @@
 package com.henrique.medical_clinic_api.exception;
 
-public class SpecialtyNotFoundException extends RuntimeException {
-  public SpecialtyNotFoundException(String message) {
-    super(message);
-  }
+import com.henrique.medical_clinic_api.exception.model.BusinessException;
+import org.springframework.http.HttpStatus;
+
+public class SpecialtyNotFoundException extends BusinessException {
+    public SpecialtyNotFoundException(String message) {
+        super(message, HttpStatus.NOT_FOUND);
+    }
 }
