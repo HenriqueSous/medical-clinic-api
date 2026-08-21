634cfeb1f0bb55347ce59a12ad34b011de881d66
Henrique Sousa <henriquesousaoliveira281@gmail.com>
2026-08-21 19:14:32 -0300

    feat(specialty service): add findAll method

--- src/main/java/com/henrique/medical_clinic_api/service/SpecialtyService.java
+++ src/main/java/com/henrique/medical_clinic_api/service/SpecialtyService.java
@@ -5,10 +5,16 @@ import com.henrique.medical_clinic_api.repository.SpecialtyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class SpecialtyService {
     @Autowired
     private SpecialtyRepository specialtyRepository;
+
+    public List<Specialty> findAll() {
+        return specialtyRepository.findAll();
+    }
 
     public Specialty save(Specialty specialty) {
         return specialtyRepository.save(specialty);
