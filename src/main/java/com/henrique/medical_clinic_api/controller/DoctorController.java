a0ad46d170d4c89bad2d7da99fc9767a292ec6ea
Henrique Sousa <henriquesousaoliveira281@gmail.com>
2026-08-20 21:53:50 -0300

    feat: add DoctorController

---
+++ src/main/java/com/henrique/medical_clinic_api/controller/DoctorController.java
@@ -0,0 +1,9 @@
+package com.henrique.medical_clinic_api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/doctors")
+public class DoctorController {
+}
