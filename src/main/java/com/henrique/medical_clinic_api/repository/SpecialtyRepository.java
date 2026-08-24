7d84d66fc74c324b939b5d47c7b3e81cb27734d5
Henrique Sousa <henriquesousaoliveira281@gmail.com>
2026-08-24 10:02:57 -0300

    remove(specialty controller): import unused removed

--- src/main/java/com/henrique/medical_clinic_api/repository/SpecialtyRepository.java
+++ src/main/java/com/henrique/medical_clinic_api/repository/SpecialtyRepository.java
@@ -4,8 +4,6 @@ import com.henrique.medical_clinic_api.model.Specialty;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface SpecialtyRepository extends JpaRepository<Specialty, Long> {
     Specialty findByNameIgnoreCase(String name);
