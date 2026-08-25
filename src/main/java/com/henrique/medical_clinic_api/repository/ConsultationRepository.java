0b184183215f5a453c525e020e6c781c245ccf44
Henrique Sousa <henriquesousaoliveira281@gmail.com>
2026-08-25 09:43:47 -0300

    feat(consultation repository): repository base created

--- src/main/java/com/henrique/medical_clinic_api/repository/ConsultationRepository.java
+++ src/main/java/com/henrique/medical_clinic_api/repository/ConsultationRepository.java
@@ -1,4 +1,9 @@
 package com.henrique.medical_clinic_api.repository;
 
-public interface ConsultationRepository {
+import com.henrique.medical_clinic_api.model.Consultation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ConsultationRepository extends JpaRepository<Consultation, Long> {
 }
