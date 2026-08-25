c3d00b003671f9529798da3621449463ba07d7c9
Henrique Sousa <henriquesousaoliveira281@gmail.com>
2026-08-25 10:00:26 -0300

    feat(consultation service): method to save consultation added

--- src/main/java/com/henrique/medical_clinic_api/service/ConsultationService.java
+++ src/main/java/com/henrique/medical_clinic_api/service/ConsultationService.java
@@ -1,4 +1,40 @@
 package com.henrique.medical_clinic_api.service;
 
+import com.henrique.medical_clinic_api.dto.consultation.ConsultationRequestDTO;
+import com.henrique.medical_clinic_api.mapper.ConsultationMapper;
+import com.henrique.medical_clinic_api.model.Consultation;
+import com.henrique.medical_clinic_api.model.Doctor;
+import com.henrique.medical_clinic_api.model.Patient;
+import com.henrique.medical_clinic_api.repository.ConsultationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ConsultationService {
+    @Autowired
+    private ConsultationRepository consultationRepository;
+
+    @Autowired
+    private ConsultationMapper consultationMapper;
+
+    @Autowired
+    private DoctorService doctorService;
+
+    @Autowired
+    private PatientService patientService;
+
+    public Consultation save(ConsultationRequestDTO consultationRequestDTO) {
+        Consultation consultation = consultationMapper.toEntity(consultationRequestDTO);
+
+        Doctor doctor = doctorService.findById(consultationRequestDTO.doctorId());
+        Patient patient = patientService.findById(consultationRequestDTO.patientId());
+
+        consultation.setDoctor(doctor);
+        consultation.setPatient(patient);
+
+        doctor.getConsultations().add(consultation);
+        patient.getConsultations().add(consultation);
+
+        return consultationRepository.save(consultation);
+    }
 }
