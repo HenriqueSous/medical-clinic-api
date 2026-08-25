02cdf8eafd6952cb9aaef9257f5e54145c61f61c
Henrique Sousa <henriquesousaoliveira281@gmail.com>
2026-08-25 09:58:44 -0300

    feat(consultation mapper): add default value to consultation status

--- src/main/java/com/henrique/medical_clinic_api/mapper/ConsultationMapper.java
+++ src/main/java/com/henrique/medical_clinic_api/mapper/ConsultationMapper.java
@@ -3,14 +3,17 @@ package com.henrique.medical_clinic_api.mapper;
 import com.henrique.medical_clinic_api.dto.consultation.ConsultationRequestDTO;
 import com.henrique.medical_clinic_api.dto.consultation.ConsultationResponseDTO;
 import com.henrique.medical_clinic_api.model.Consultation;
+import com.henrique.medical_clinic_api.model.ConsultationStatus;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring", uses = {DoctorMapper.class, PatientMapper.class, SpecialtyMapper.class})
+@Mapper(componentModel = "spring", uses = {DoctorMapper.class, PatientMapper.class, SpecialtyMapper.class, ConsultationStatus.class})
 public interface ConsultationMapper {
     ConsultationMapper INSTANCE = Mappers.getMapper(ConsultationMapper.class);
 
     ConsultationResponseDTO toResponse(Consultation consultation);
 
+    @Mapping(target = "status", expression = "java(ConsultationStatus.SCHEDULED)")
     Consultation toEntity(ConsultationRequestDTO consultationRequestDTO);
 }
