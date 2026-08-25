351ec1c8cf5f0e4dd67563d8ea5da9a45e7a5bf5
Henrique Sousa <henriquesousaoliveira281@gmail.com>
2026-08-25 09:37:33 -0300

    feat(consultation): endpoint to post consultation added

--- src/main/java/com/henrique/medical_clinic_api/controller/ConsultationController.java
+++ src/main/java/com/henrique/medical_clinic_api/controller/ConsultationController.java
@@ -1,4 +1,27 @@
 package com.henrique.medical_clinic_api.controller;
 
+import com.henrique.medical_clinic_api.dto.consultation.ConsultationRequestDTO;
+import com.henrique.medical_clinic_api.dto.consultation.ConsultationResponseDTO;
+import com.henrique.medical_clinic_api.mapper.ConsultationMapper;
+import com.henrique.medical_clinic_api.service.ConsultationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/consultations")
 public class ConsultationController {
+    @Autowired
+    private ConsultationMapper consultationMapper;
+
+    @Autowired
+    private ConsultationService consultationService;
+
+    @PostMapping
+    private ResponseEntity<ConsultationResponseDTO> post(@RequestBody ConsultationRequestDTO consultationRequestDTO) {
+        return ResponseEntity.ok(consultationMapper.toResponse(consultationService.save(consultationRequestDTO)));
+    }
 }
