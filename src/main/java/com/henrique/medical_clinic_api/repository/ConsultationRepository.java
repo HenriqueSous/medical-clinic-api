7fa367ca517a5084e99b626fc1ace3b125dfd79d
Henrique Sousa <henriquesousaoliveira281@gmail.com>
2026-08-25 09:36:13 -0300

    feat(consultation): ConsultationRepository and ConsultationService added

---
+++ src/main/java/com/henrique/medical_clinic_api/repository/ConsultationRepository.java
@@ -0,0 +1,4 @@
+package com.henrique.medical_clinic_api.repository;
+
+public interface ConsultationRepository {
+}
---
+++ src/main/java/com/henrique/medical_clinic_api/service/ConsultationService.java
@@ -0,0 +1,4 @@
+package com.henrique.medical_clinic_api.service;
+
+public class ConsultationService {
+}
