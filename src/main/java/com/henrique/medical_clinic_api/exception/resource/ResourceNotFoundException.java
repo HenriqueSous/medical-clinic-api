571f05de2eff735afe6c2b3db95d1fa4dfbf2ad9
Henrique Sousa <henriquesousaoliveira281@gmail.com>
2026-09-05 10:28:48 -0300

    feat(exception): generic exception for not found resources added

---
+++ src/main/java/com/henrique/medical_clinic_api/exception/resource/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.henrique.medical_clinic_api.exception.resource;
+
+import com.henrique.medical_clinic_api.exception.domain.BusinessException;
+import org.springframework.http.HttpStatus;
+
+public class ResourceNotFoundException extends BusinessException {
+    public ResourceNotFoundException(String resourceName, long id) {
+        super(String.format("%s with id '%d' not found", resourceName, id),
+                HttpStatus.NOT_FOUND);
+    }
+}
