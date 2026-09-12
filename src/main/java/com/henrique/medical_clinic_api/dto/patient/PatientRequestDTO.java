cd3e1418e297da57ed272f17a84444597e35b3fd
Henrique Sousa <henriquesousaoliveira281@gmail.com>
2026-09-12 11:07:32 -0300

    feat(patient): 11 digit validation added to the cpf

--- src/main/java/com/henrique/medical_clinic_api/dto/patient/PatientRequestDTO.java
+++ src/main/java/com/henrique/medical_clinic_api/dto/patient/PatientRequestDTO.java
@@ -1,11 +1,14 @@
 package com.henrique.medical_clinic_api.dto.patient;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record PatientRequestDTO(
         @NotBlank(message = "The patient name is mandatory")
         String name,
         @NotBlank(message = "The patient cpf is mandatory")
+        @Pattern(regexp = "\\d{11}", message = "Invalid CPF")
         String cpf
 ) {
 }
